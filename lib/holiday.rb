0ab33cbd0678324f9ce29d6a3e2b2868db4f438c
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 22:38:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, supplies|
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
