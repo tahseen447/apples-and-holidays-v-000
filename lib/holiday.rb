46edfa2437e822164a106c636eadd69535f1ffaf
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 23:17:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    if season == :new_years
+    if season === :new_years
       puts "New Years:"
     else
     puts "#{season.capitalize}:"
