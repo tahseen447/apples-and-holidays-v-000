6aee8547ea3550c8d3aa17d01ed18bf6ede67f57
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 23:11:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |name, supplies|
-      puts "#{name.capitalize}:"
+      puts " #{name.capitalize}:"
       supplies.each do |item|
         puts "#{item}"
       end
